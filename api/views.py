aeee03c3e7f21ea5848914839d0ea62e2b7a01c3
Fadhel <fadhel.s.hashem@gmail.com>
2026-08-24 16:23:18 +0300

    JWT views part 1

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+from .serializers import UserSerializer
+
+def create_access_token(user):
+    token = RefreshToken.for_user(user).access_token
+    token["payload"] = {
+        "_id": str(user.id),
+        "username": user.username,
+    }
+    return str(token)
